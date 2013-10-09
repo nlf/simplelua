144eb509b0e90de4d0495d9c39b79a7fb3dc95de
Nathan LaFreniere <quitlahok@gmail.com>
2013-10-08 21:59:13 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+SimpleLua
+=========
+
+This module was written to provide a very simple wrapper for the redis library to add lua scripts as members of the prototype.
+
+Usage
+-----
+
+```./lua/print.lua```
+
+```lua
+-- all arguments passed to the function will be in the ARGV table, in order
+return ARGV[1]
+```
+
+```client.js```
+
+```js
+// the scriptPath option is optional and defaults to './lua'
+// it specifies what directory contains your lua scripts
+// all other parameters are identical to the node_redis module
+
+var client = require('simplelua').createClient(6379, '127.0.0.1', { scriptPath: './lua' });
+
+// since we loaded the file print.lua, we now have client.print available
+client.print('testing', function (err, reply) {
+  console.log(reply); // "testing"
+});
+```
+
+Security Considerations
+-----------------------
+
+This module *will* allow you to overwrite native redis commands and potentially completely break the module or destroy your data. Consider yourself warned.
---
+++ index.js
@@ -0,0 +1,47 @@
+var net = require('net');
+var util = require('util');
+var fs = require('fs');
+var path = require('path');
+var crypto = require('crypto');
+var redis = require('redis');
+
+function SimpleRedis(client, options) {
+    var self = this;
+
+    options = options || {};
+    var scriptPath = options.scriptPath || './lua';
+    delete options.scriptPath;
+
+    redis.RedisClient.call(this, client, options);
+
+    var files = fs.readdirSync(scriptPath);
+    var name;
+    for (var i = 0, l = files.length; i < l; i++) {
+        if (path.extname(files[i]).toLowerCase() === '.lua') {
+            name = path.basename(files[i], '.lua');
+            SimpleRedis.prototype[name] = (function (name, script) {
+                return function () {
+                    var args = Array.prototype.slice.call(arguments);
+                    var callback;
+
+                    if (typeof args[args.length - 1] === 'function') callback = args.pop();
+                    return self.eval(script, 0, args, callback);
+                };
+            })(name, fs.readFileSync(path.join(scriptPath, files[i]), 'utf8'));
+        }
+    }
+}
+
+util.inherits(SimpleRedis, redis.RedisClient);
+
+exports.createClient = function (port, host, options) {
+    port = port || 6379;
+    host = host || '127.0.0.1';
+
+    var client = net.createConnection(port, host);
+    var r = new SimpleRedis(client, options);
+    r.port = port;
+    r.host = host;
+
+    return r;
+};
